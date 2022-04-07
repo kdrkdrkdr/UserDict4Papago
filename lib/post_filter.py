2c2fde24b5bdbdee0929fedb1f80b1c905a384e5
kdrkdrkdr <kdrhacker1234@gmail.com>
2022-04-07 13:22:58 +0900

    edit files

--- lib/post_filter.py
+++ lib/post_filter.py
@@ -5,12 +5,12 @@ from lib.util import ReplaceText
 class PostFilter:
     
 
-    def __init__(self, text: str, dict_list:dict, repl_list:list):
+    def __init__(self, text: str, dict_list:dict):
         self.text = text
         self.dict_list = dict_list
-        self.repl_list = repl_list
 
     def post_process(self):
+        # 옛날 발음 나는 애들 어떻게 처리할까...
         post = ReplaceText(
             self.text, {
                 '｣':'"',
--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def main():
 
     dict_list = ConvertDictionary().convert()
     pre = PreFilter(text, dict_list).pre_process()
-    trans = PapagoTranslator(pre[0], 10).translate()
-    post = PostFilter(trans, dict_list, pre[1]).post_process()
+    trans = PapagoTranslator(pre, 10).translate()
+    post = PostFilter(trans, dict_list).post_process()
 
-    WriteFile(pre[0], './result/t-pre.txt') # 전처리
+    WriteFile(pre, './result/t-pre.txt') # 전처리
     WriteFile(trans, './result/t-trans.txt') # 파파고 번역
     WriteFile(post, './result/t-post.txt') # 후처리
+
+
+
+
 
 
 import cProfile
